ee0f83ff7c6e4ab955c49d027134d387e1ef7e8e
zakzc <zak.zangrando@outlook.com>
2022-09-16 19:48:14 +0200

    Finaliza o programa com input e output da resposta

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const prompt = require('prompt-sync')();
 const peso = prompt('Qual o seu peso? ');
 console.log(`O peso informado foi de ${peso} quilos`);
 
-const altura = prompt('Qual a sua altura (em centímetros)? ');
-console.log(`A altura informada foi de ${altura} centímetros`);
+const altura = prompt('Qual a sua altura (em metros, com ponto divisor)? ');
+console.log(`A altura informada foi de ${altura} `);
+
+const imc = peso / (altura * altura)
+console.log(`\nSeu IMC é ${imc}.\n`)
